9c1e559ec06059298ba9fca8f0c5080f9cd19fcb
Diego <dgarciacosio@gmail.com>
2020-08-23 10:46:14 +0200

    Añadidas primeras pruebas con JSON y devolviendo tipos propios

--- run.js
+++ run.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
+const fs = require('fs');
+
+const jsonString = fs.readFileSync('./familia.json')
+const familia = JSON.parse(jsonString)
 
 var schema = buildSchema(`
+  type Miembro{
+    name:String!
+    age: Int!
+  }
   type Query {
-    hello: String
+    getFamilyNumber(name:String!): Miembro
   }
 `);
 
-var root = { hello: () => 'Hello world!' };
+var root = {
+  getFamilyNumber:({name}) => {
+    for (let index = 0; index < familia.length; index++) {
+      if(familia[index].name == name)
+        return familia[index];
+    }
+
+    return{name:"ERROR", age:-1};
+  }
+}
+
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
